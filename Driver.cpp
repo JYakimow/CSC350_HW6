6f017c5e31794d6b36a2950e385c721cf0f94771
JYakimow <jdyakimow@gmail.com>
2021-02-22 23:26:49 -0600

    update

--- Driver.cpp
+++ Driver.cpp
@@ -2,16 +2,27 @@
 #include <string.h>
 using namespace std;
 
-string a = "";
+//string jsonInput = "[{“fname”:”val1”, “lname”: “val2”, “sums”:[1,2,3]},{“fname”:”val3”, “lname”:”val4”}]";
+string jsonInput = "hello";
 
-string convertToInt(string jsonInput)
+string parseJSON(string jsonInput)
 {
-    return "test";
+    //char jsonStringArr [jsonInput.length()];
+    int jsonInputLength = jsonInput.length();
+    for(int i = 0; i < jsonInputLength; i = i + 1)
+    {
+        //jsonStringArr[i] = jsonInput[i];
+        cout << jsonInput[i] << endl;
+    }
+    
+    return "wip test";
 }
 
 int main(int argc, char** argv)
 {
-    cout << "hello world" << endl;
+    cout << "main hello world" << endl;
+    //cout << parseJSON("a") << endl;
+    parseJSON(jsonInput);
     
     return 0;
 }
